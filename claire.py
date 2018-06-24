7cdbe4dabbdfc94cb887e38d0fcb559b9c8cf099
Frederick <fredericklcho@gmail.com>
2018-06-24 01:00:32 -0400

    changed conversation to a function

--- claire.py
+++ claire.py
@@ -3,12 +3,25 @@ import pyttsx3
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-engine = pyttsx3.init()
-while True:
-    with mic as source:
-        audio = r.listen(source)
 
-#print(r.recognize_google(audio))
-    if r.recognize_google(audio) == "hi Claire":
-        engine.say("Hello, how are you?")
-        engine.runAndWait()
+def conversation(audio,engine):
+   if r.recognize_google(audio) == "hi Claire":
+          print("lala")
+          engine.say("Hello, how are you Frederick?")
+          engine.runAndWait()
+          audio = r.listen(source)
+          print(r.recognize_google(audio))
+          if r.recognize_google(audio) == "I'm good":
+             engine.say("that's great!")
+             engine.runAndWait()
+
+
+#greetings = ['hi Claire','hey Claire',
+engine = pyttsx3.init()
+with mic as source:
+    while True:
+       audio = r.listen(source)
+       conversation(audio,engine)
+
+
+       
